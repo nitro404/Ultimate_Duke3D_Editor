287cda74f2a984162360112c213a7432cb3cf48f
Kevin Scroggins <nitro404@gmail.com>
2015-05-01 18:13:21 -0400

    Disabled transfer handler for console text area for now, since it prevented copy + pasting of text

--- Group Manager/src/gui/GroupManagerWindow.java
+++ Group Manager/src/gui/GroupManagerWindow.java
@@ -406,7 +406,7 @@ public class GroupManagerWindow implements WindowListener, ComponentListener, Ch
 		m_consoleFont = new Font("Verdana", Font.PLAIN, 14);
 		m_consoleText.setFont(m_consoleFont);
 		m_consoleText.setEditable(false);
-		m_consoleText.setTransferHandler(m_transferHandler);
+//		m_consoleText.setTransferHandler(m_transferHandler);
 		m_consoleScrollPane = new JScrollPane(m_consoleText);
 		m_mainTabbedPane.add(m_consoleScrollPane);
 		
